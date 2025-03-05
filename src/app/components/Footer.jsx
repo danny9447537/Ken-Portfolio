ad490a4a55707ec5cc8ee967f65af08e9b088656
dken-9447 <dken@mail.valenciacollege.edu>
2025-03-04 22:59:14 -0500

    fixed footer responsiveness on small and extra large device screens

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <footer className="footer border-t border-l border-r border-[#33353F] z-10 text-white">
-            <div className="container p-12 flex justify-between">
+            <div className="w-full max-w-screen-xl mx-auto p-6 md:p-12 flex flex-col md:flex-row justify-between items-center text-center md:text-left">
                 <span>
                     <Image
                         src="/images/dannykenlogo.png"
                         alt="Logo"
-                        width={250} // Adjust the width as needed
-                        height={50} // Adjust the height as needed
+                        width={250}
+                        height={50}
                         className="cursor-pointer"
                     />
                 </span>
-                <p className="text-slate-600">All rights reserved.</p>
+                <p className="text-slate-600 mt-4 md:mt-0 md:self-end">All rights reserved.</p>
             </div>
         </footer>
     );
