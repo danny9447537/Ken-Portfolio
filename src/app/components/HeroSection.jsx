a24456248005683e7acbd766b67d2a03376c805f
dken-9447 <dken@mail.valenciacollege.edu>
2025-03-05 15:10:23 -0500

    adjusted hero-image.png to add more space

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
-                    className="col-span-4 place-self-center mt-4 lg:mt-0">
+                    className="col-span-4 place-self-center mt-8 lg:mt-0">
                     <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
                             src="/images/hero-image.png"
