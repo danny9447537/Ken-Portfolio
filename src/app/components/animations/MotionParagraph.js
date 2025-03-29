4f332da812ea407caa022884053386fd6345d71a
dken-9447 <dken@mail.valenciacollege.edu>
2025-03-29 14:48:06 -0400

    fixed layout issue on mobile

--- src/app/components/animations/MotionParagraph.js
+++ src/app/components/animations/MotionParagraph.js
@@ -7,8 +7,8 @@ const MotionParagraph = ({ children, index }) => {
     return (
         <motion.p
             className="text-base lg:text-lg"
-            initial={{ opacity: 0, x: isEven ? -150 : 150, y: 40 }}
-            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
             transition={{
                 delay: index * 0.1,
                 duration: 0.7,
